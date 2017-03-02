cb9690ddf34f2c3d5a892d9920296f7be7417d50
Kelly <kmmccallan@gmail.com>
2017-03-02 12:46:54 -0500

    Door organization

---
+++ AnimationTutorial/Assets/Door Animation/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator animator;
+    public Rect openDoorRectangle;
+    public Rect closeDoorRectangle;
+
+    void OnGUI()
+    {
+        if (GUI.Button(openDoorRectangle, "Open Door"))
+        {
+            animator.SetInteger("Door Open", 1);
+            animator.SetInteger("Door Close", 0);
+        }
+        if (GUI.Button(closeDoorRectangle, "Close Door"))
+        {
+            animator.SetInteger("Door Open", 0);
+            animator.SetInteger("Door Close", 1);
+        }
+    }
+}
